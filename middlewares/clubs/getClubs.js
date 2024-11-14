cd043b8270708a7710fb19a088a1d2008d1d85dc
tumonulo <166932691+tumonulo@users.noreply.github.com>
2024-11-14 20:31:00 +0100

    d

---
+++ middlewares/clubs/getClubs.js
@@ -0,0 +1,23 @@
+require("discord.js");
+require("dotenv").config();
+const BRAWLSTARSAPIKEY = process.env.BRAWLSTARSAPIKEY;
+const guild = client.guilds.cache.get("dsds");
+const channel = guild.channels.cache.get("fsdfw");
+const message = await channel.messages.fetch({ limit: 1 }).first();
+
+async function getClubes() {
+    let clubes = [];
+    message.fields.forEach(field => {
+        const clubID = field.name.replace(/fdsf|dfd/, "");
+        const request = await fetch(`https://api.brawlstars.com/v1/clubs/%23${clubID}`, {
+            headers: {
+                "Authorization": `Bearer ${BRAWLSTARSAPIKEY}`
+            }
+        })
+        const response = await request.json();
+        clubes.push(response);
+    });
+    return clubes;
+}
+
+module.exports = getClubes();
---
+++ middlewares/clubs/statsClubs.js
@@ -0,0 +1,15 @@
+const clubs = require("./getClubs.js");
+
+function statsClubs() {
+    let statsClubs = ""
+    clubs.forEach(club, index => {
+        statsClubs = statsClubs + `
+       `
+
+        if (clubs.length != index) {
+            //2
+        }
+    })
+}
+
+module.exports = statsClubs()
