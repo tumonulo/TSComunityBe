611c8c01fdde66f6d49276be0c7b623f77defae6
tumonulo <166932691+tumonulo@users.noreply.github.com>
2024-12-11 15:02:06 +0100

    Create overlayMessage

--- controllers/overlay/overlayMessage.js
+++ controllers/overlay/overlayMessage.js
@@ -1,6 +1,31 @@
 const { client } = require('../../ready.js')
 
 module.exports = async function overlay(req, res) {
-    const message = 'hola';
-    res.json({ message });
+    const messages = []
+    const guild = client.guilds.cache.get('ID del guild')
+    const channel = guild.channels.cache.get('ID del canal')
+    const message = channel.messages.first()
+    const embed = message.embeds.first()
+    const fields = embed.fields
+
+    const memberCount = guild.memberCount
+
+    aysnc function getActiveMembers() {
+        guild.members.fetch({ withPresences: true }).then(members => {
+            const activeMembers = members.filter(member => 
+                member.presence?.status === 'online' || 
+                member.presence?.status === 'dnd' || 
+                member.presence?.status === 'idle').size
+                return activeMembers
+        })
+    }
+    
+    const activeMembers = getActiveMembers()
+
+    const clubsCount = fields.length
+
+    messages.push(memberCount, activeMembers, clubsCount)
+    const randomIndex = Math.floor(Math.random() * messages.length);
+    const response = array[randomIndex];
+    res.json({ response });
 };
